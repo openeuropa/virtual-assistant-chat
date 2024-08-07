9e73574ce11f6ace03ec81623ca0b3b83daafbbf
Antonio De Marco <antonio@nuvole.org>
2024-08-07 15:25:16 +0200

    D8TSDT-518: Add vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // eslint-disable-next-line no-undef
+      entry: resolve(__dirname, "src/VAEmbeddableChat.jsx"),
+      name: "VAEmbeddableChat",
+      fileName: "va-embeddable-chat",
+    },
+  },
 });
