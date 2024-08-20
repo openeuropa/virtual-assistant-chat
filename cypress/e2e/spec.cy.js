7e446456fc94aebdc551bd3684d2769f519f9188
Antonio De Marco <antonio@nuvole.org>
2024-08-20 10:53:48 +0200

    D8TSDT-525: Add cypress specs.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('visit chat', () => {
+  it('should visit', () => {
+    cy.visit('/')
   })
 })
