9a018965ca403b6ed34c210b2ba6878f3236342a
Antonio De Marco <antonio@nuvole.org>
2024-08-22 10:55:20 +0200

    D8TSDT-525: Add first cypress test.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,26 @@
-describe('visit chat', () => {
-  it('should visit', () => {
-    cy.visit('/')
-  })
-})
+describe("visit chat", () => {
+  it("should visit", () => {
+    // Mock request with fixture.
+    cy.intercept("GET", "/ask?question=*", { fixture: "ask.json" }).as(
+      "getAnswer",
+    );
+
+    // Type in message area.
+    cy.visit("/");
+    cy.get("[contenteditable]")
+      .first() // Adjust the selector as needed
+      .focus()
+      .type("Your text here");
+
+    // Send message.
+    cy.get("button.cs-button.cs-button--send").click();
+
+    // Assert that chat displays mocked response correctly.
+    cy.wait("@getAnswer").then((interception) => {
+      cy.get(".cs-message.cs-message--incoming").should(
+        "include.text",
+        "This is a mocked answer",
+      );
+    });
+  });
+});
