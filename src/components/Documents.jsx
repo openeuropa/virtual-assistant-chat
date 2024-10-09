2e8b5a6af052e058112442741a724f327fa3ebd2
Antonio De Marco <antonio@nuvole.org>
2024-10-09 10:30:21 +0200

    D8TSDT-547: Make document section expandable.

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Document } from "@/components/Document.jsx";
+import docsIcon from "@/assets/docs-icon.svg?raw";
 
-export const Documents = ({ documents }) => {
+export const Documents = ({ documents, expanded = false }) => {
+  const [toggle, setToggle] = useState(expanded);
+
   return (
     <div className="nlux-documents">
-      <b>Supporting documents</b>
-      {documents.map((document) => (
-        <Document key={document.id} document={document} />
-      ))}
+      <button
+        className="nlux-documents-toggle"
+        onClick={() => setToggle(!toggle)}
+      >
+        <img
+          className="nlux-documents-icon"
+          src={`data:image/svg+xml;base64,${btoa(docsIcon)}`}
+        />
+        <div>
+          <b>{documents.length}</b> supporting documents
+        </div>
+        <div
+          className={`nlux-doc-toggle nlux-doc-toggle-small${toggle ? " nlux-doc-toggle-active" : ""}`}
+        ></div>
+      </button>
+
+      {toggle && (
+        <>
+          {documents.map((document) => (
+            <Document key={document.id} document={document} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
